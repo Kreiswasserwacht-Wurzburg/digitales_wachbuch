cdb4b9f293f4defb25c6fb4e0d3ffac5631fa501
Philipp Jeske <philipp.jeske@wasserwacht.bayern>
2020-12-27 21:14:59 +0100

    Clean up code structure and split up in separate units (#24)
    * Add initial code structure
    
    * Stylesheet mit Sass implementiert
    
    * Integrate style into clean structure and migrate to .NET 5.0
    
    * Update .travis.yml
    
    * Remove unneccessary files
    
    * Update Bootstrap from v4.x -> v5.
    
    * Clean up structure
    
    * Replace fixed station content with variable data
    
    Co-authored-by: Thomas Scheuermann <th.scheuermann@web.de>

---
+++ src/Wasserwacht.DigitalGuardBook.Common.Logic/Repositories/StationRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wasserwacht.DigitalGuardBook.Common.Logic.Repositories
+{
+    public class StationRepository
+    {
+        public async Task<Station> GetStationAsync()
+        {
+            return new Station
+            {
+                Name = "WRST Würzburg",
+                PhoneNumber = "0931 800080",
+                Street = "Mergentheimer Straße 9a",
+                ZipCode = "97082",
+                City = "Würzburg"
+            };
+        }
+    }
+}
