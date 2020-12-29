b17f410a4c4e76e0b8413b56885df18a06594dac
Philipp Jeske <philipp.jeske@wasserwacht.bayern>
2020-12-29 22:08:10 +0100

    Enable pages and views within Razor Class Libs
    Further information: https://docs.microsoft.com/de-de/aspnet/core/razor-pages/ui-class?view=aspnetcore-5.0&tabs=visual-studio

--- src/Wasserwacht.DigitalGuardBook.Common.Ui/Areas/Common/Station/StationInfo.razor.cs
+++ src/Wasserwacht.DigitalGuardBook.Common.Ui/Areas/Common/Station/StationInfo.razor.cs
@@ -1,6 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
-namespace Wasserwacht.DigitalGuardBook.Common.Ui
+namespace Wasserwacht.DigitalGuardBook.Common.Ui.Areas.Common.Station
 {
     public partial class StationInfo
     {
