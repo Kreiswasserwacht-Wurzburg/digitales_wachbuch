133b2b26e030e1a55b9a27a81bda51b44b0909d7
Philipp Jeske <philipp.jeske@wasserwacht.bayern>
2020-12-29 23:17:26 +0100

    Start to implement station edit form

---
+++ src/Wasserwacht.DigitalGuardBook.Common.Ui/Areas/Common/Station/Station.razor.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wasserwacht.DigitalGuardBook.Common.Ui.Areas.Common.Station
+{
+    public class StationModel
+    {
+        [Required]
+        [MaxLength(250)]
+        public string Name { get; set; }
+
+        [MaxLength(500)]
+        public string Street { get; set; }
+
+        [MaxLength(20)]
+        public string ZipCode { get; set; }
+
+        [MaxLength(100)]
+        public string City { get; set; }
+
+        [Phone]
+        public string PhoneNumber { get; set; }
+    }
+
+    public partial class Station
+    {
+        private DigitalGuardBook.Common.Data.Station station = new DigitalGuardBook.Common.Data.Station();
+        private EditContext editContext;
+
+        private async Task HandleValidSubmit(EditContext editContext)
+        {
+            editContext.NotifyValidationStateChanged();
+        }
+
+        protected async override Task OnInitializedAsync()
+        {
+            editContext = new EditContext(station);
+            editContext.SetFieldCssClassProvider(new BootstrapClassProvider());
+
+            station = await StationService.GetStationAsync();
+        }
+    }
+}
---
+++ src/Wasserwacht.DigitalGuardBook.Common/BootstrapClassProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Components.Forms;
+
+
+namespace Wasserwacht.DigitalGuardBook.Common
+{
+    public class BootstrapClassProvider : FieldCssClassProvider
+    {
+        public override string GetFieldCssClass(EditContext editContext, in FieldIdentifier fieldIdentifier)
+        {
+            var isValid = !editContext.GetValidationMessages(fieldIdentifier).Any();
+
+            return isValid ? "is-valid" : "is-invalid";
+        }
+    }
+}
