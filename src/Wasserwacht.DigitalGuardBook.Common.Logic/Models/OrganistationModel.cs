64e10e408d1433476e70925be811baf3be21cfc0
Philipp Jeske <philipp.jeske@wasserwacht.bayern>
2021-07-18 15:52:30 +0200

    Start to implement organsitions

---
+++ src/Wasserwacht.DigitalGuardBook.Common.Logic/Models/OrganistationModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wasserwacht.DigitalGuardBook.Common.Logic.Models
+{
+    class OrganistationModel
+    {
+        public Guid? Id { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public string Name { get; set; }
+
+        [Required]
+        public ushort? Number { get; set; }
+    }
+}
---
+++ src/Wasserwacht.DigitalGuardBook.Common.Logic/Services/OrganisationService.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wasserwacht.DigitalGuardBook.Common.Data;
+
+namespace Wasserwacht.DigitalGuardBook.Common.Logic.Services
+{
+    class OrganisationService : BaseService
+    {
+        public OrganisationService(CommonDataContext commonDataContext) : base(commonDataContext)
+        {
+        }
+
+        public async Task<List<Models.OrganistationModel>> GetOrganisationsAsync()
+        {
+            return await _commonDataContext.Organisations
+                .OrderBy(x => x.Name)
+                .Select(x => new Models.OrganistationModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Number = x.Number
+                }).ToListAsync();
+        }
+
+
+        public async Task<Models.OrganistationModel> GetOrganisationAsync(Guid id)
+        {
+            Data.Organisation dbOrganisation;
+
+            if (!await _commonDataContext.Organisations.AnyAsync(x => x.Id == id))
+            {
+                dbOrganisation = null;
+            }
+            else
+            {
+                dbOrganisation = await _commonDataContext.Organisations.FirstOrDefaultAsync(x => x.Id == id);
+            }
+
+            return new Models.OrganistationModel
+            {
+                Id = dbOrganisation?.Id,
+                Name = dbOrganisation?.Name,
+                Number = dbOrganisation?.Number,
+            };
+        }
+    }
+}
