271949a53a5287551e0a11f58c11459562cc7738
Philipp Jeske <philipp.jeske@wasserwacht.bayern>
2021-12-21 18:00:22 +0100

    Customized identity pages to our needs

---
+++ src/Wasserwacht.DigitalGuardBook.Ui/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Wasserwacht.DigitalGuardBook.Common.Data;
+
+[assembly: HostingStartup(typeof(Wasserwacht.DigitalGuardBook.Ui.Areas.Identity.IdentityHostingStartup))]
+namespace Wasserwacht.DigitalGuardBook.Ui.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
