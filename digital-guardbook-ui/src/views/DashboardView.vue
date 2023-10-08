af3b481b00f0ef6b08dd1792ba1ded26edf8c4a6
Philipp Jeske <philipp.jeske@wasserwacht.bayern>
2023-10-08 12:18:03 +0200

    Remove flex classes, because the scrolling won't work as expected

--- digital-guardbook-ui/src/views/DashboardView.vue
+++ digital-guardbook-ui/src/views/DashboardView.vue
@@ -5,13 +5,13 @@ import SentryCard from "@/components/sentry/SentryCard.vue"
 </script>
 
 <template>
-  <main class="flex-fill d-flex">
-    <div class="container-fluid flex-fill d-flex">
-      <div class="row my-3 flex-fill">
+  <main>
+    <div class="container-fluid">
+      <div class="row my-3">
 
         <SentryCard />
         
-        <div class="col-md-3 d-flex flex-column justify-content-around">
+        <div class="col-md-3 justify-content-around">
           <WeatherCard :station-id=10655 />
           <div class="card">
             <div class="card-body">
