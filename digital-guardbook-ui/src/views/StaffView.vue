7add801c37ac1b66bc0b54e622885f551aeecbf5
Philipp Jeske <philipp.jeske@wasserwacht.bayern>
2024-02-25 19:19:07 +0100

    Fix integration of PickList in StaffView

--- digital-guardbook-ui/src/views/StaffView.vue
+++ digital-guardbook-ui/src/views/StaffView.vue
@@ -1,35 +1,38 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
-
-const { t } = useI18n({
-    useScope: 'global'
-})
-
 import { usePersonStore } from '@/store/person'
 import { storeToRefs } from 'pinia'
-import { onMounted } from 'vue'
 import PickList from '@/components/data/PickList.vue'
 import { useSentryStore } from '../store/sentry';
-
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faFloppyDisk } from '@fortawesome/free-solid-svg-icons'
 import type { Person } from '@/models/person';
+import { ref, watch, computed, onMounted } from 'vue'
+import { onBeforeRouteLeave } from 'vue-router'
 import { watch, computed } from 'vue';
 import { onBeforeRouteLeave } from 'vue-router';
 
 library.add(faFloppyDisk)
 
+const { t } = useI18n({
+    useScope: 'global'
+})
+
 const store = usePersonStore()
 const sentryStore = useSentryStore()
 const { loading, persons } = storeToRefs(store)
+const dialog = ref<InstanceType<typeof ModalDialog> | null>()
+
 const { guards } = storeToRefs(sentryStore)
+
+const localGuardList = ref<Array<Person>>([])
 
 let _pristine: Person[] = []
 let _isPristineSet = false
 
 onMounted(() => {
-    store.fetchAll()
     sentryStore.getActiveSentry()
+    store.fetchAll()
 })
 
 watch(guards, (newValue, oldValue) => {
@@ -37,14 +40,16 @@ watch(guards, (newValue, oldValue) => {
         newValue.forEach(val => _pristine.push(Object.assign({}, val)));
         _isPristineSet = true
     }
+
+    localGuardList.value = newValue
 })
 
 var isModified = computed(() => {
-    if (_pristine === guards.value) { return false }
-    if (_pristine == null || guards.value == null) { return true }
-    if (_pristine.length !== guards.value.length) { return true }
+    if (_pristine === localGuardList.value) { return false }
+    if (_pristine == null || localGuardList.value == null) { return true }
+    if (_pristine.length !== localGuardList.value?.length) { return true }
 
-    for (const guard of guards.value) {
+    for (const guard of localGuardList.value) {
         var item = _pristine.find((x) => x.id == guard.id)
 
         if (!item) {
@@ -55,13 +60,12 @@ var isModified = computed(() => {
     return false
 })
 
-onBeforeRouteLeave((to, from)=> {
+onBeforeRouteLeave((to, from) => {
     return confirm('Do you really want to leave? you have unsaved changes!')
 })
 
 function onBeforeUnload(ev: BeforeUnloadEvent) {
-    if (isModified.value)
-    {
+    if (isModified.value) {
         ev.preventDefault()
         ev.returnValue = "Unsaved changes"
     }
@@ -69,37 +73,38 @@ function onBeforeUnload(ev: BeforeUnloadEvent) {
 
 window.onbeforeunload = onBeforeUnload
 
-function getAddedGuards(): Array<Person> {
+var addedGuards = computed(() => {
     var result = Array<Person>()
 
-    for (const guard of guards.value) {
-        var item = _pristine.find((x) => x.id == guard.id)
-        if (!item) {
-            result.push(guard)
+    if (localGuardList.value) {
+        for (const guard of localGuardList.value) {
+            var item = _pristine.find((x) => x.id == guard.id)
+            if (!item) {
+                result.push(guard)
+            }
+        }
+
+    }
+    return result
+})
+
+var removedGuards = computed(() => {
+    var result = Array<Person>()
+
+    if (localGuardList.value) {
+        for (const guard of _pristine) {
+            var item = localGuardList.value.find((x) => x.id == guard.id)
+            if (!item) {
+                result.push(guard)
+            }
         }
     }
 
     return result
-}
-
-function getRemovedGuards(): Array<Person> {
-    var result = Array<Person>()
-
-    for (const guard of _pristine) {
-        var item = guards.value.find((x) => x.id == guard.id)
-        if (!item) {
-            result.push(guard)
-        }
-    }
-
-    return result
-}
+})
 
 function save() {
-    var inserts = getAddedGuards()
-    var deletes = getRemovedGuards()
 
-    confirm(`${inserts.length} guards added and ${deletes.length} guards removed`)
 }
 </script>
 
@@ -107,7 +112,7 @@ function save() {
     <main>
         <div class="container-fluid">
             <div class="row my-3">
-                <PickList v-model:data="persons" v-model:selection="guards" v-if="!loading && persons">
+                <PickList :dataSource="persons" v-model:selection="localGuardList" v-if="!loading && persons && guards">
                     <template #targetTitle>
                         <button class="float-end btn btn-outline-secondary" type="button" @click="save"><font-awesome-icon
                                 :icon="['fa', 'floppy-disk']" /> </button>
