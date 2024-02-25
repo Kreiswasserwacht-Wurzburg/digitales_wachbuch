262be4d138ee9e56c89d08d258a84295260d71cf
Philipp Jeske <philipp.jeske@wasserwacht.bayern>
2024-02-25 14:04:01 +0100

    Make dialog component configurable

--- digital-guardbook-ui/src/components/dialog/ModalDialog.vue
+++ digital-guardbook-ui/src/components/dialog/ModalDialog.vue
@@ -1,34 +1,82 @@
 <script setup lang="ts">
+/// <reference types="bootstrap" />
 import { ref, onMounted } from 'vue'
-import { Modal } from 'bootstrap/js/index.esm'
+import { Modal } from 'bootstrap'
+import { useI18n } from 'vue-i18n'
 
-export interface Options {
+const { t } = useI18n({
+    useScope: 'global'
+})
+
+export interface DialogConfig {
     backdrop: boolean | 'static',
     focus: boolean,
     keyboard: boolean
 }
 
-const dialogElem = ref<HTMLElement| null>(null)
+export interface Props {
+    withHeader: boolean,
+    submitCaption: string,
+    cancelCaption: string,
+    autoCloseOnAbort: boolean
+}
 
-const modal = ref<Modal>();
+const props = withDefaults(defineProps<Props>(), {
+    withHeader: true,
+    submitCaption: 'common.buttons.submit',
+    cancelCaption: 'common.buttons.cancel',
+    autoCloseOnAbort: true
+})
 
-const open = () => (modal.value.show())
-const close = () => (modal.value.hide())
+const dialogElem = ref<HTMLElement | null>(null)
+
+const open = () => {
+    if (dialogElem.value != null) {
+        Modal.getInstance(dialogElem.value)?.show()
+    }
+}
+const close = () => {
+    if (dialogElem.value != null) {
+        Modal.getInstance(dialogElem.value)?.hide()
+    }
+}
 
 defineExpose({
     open, close
 })
 
 onMounted(() => {
-    modal.value = new Modal(dialogElem.value)
+    const config: DialogConfig = {
+        backdrop: 'static',
+        focus: true,
+        keyboard: true,
+    }
+
+    if (dialogElem.value != null) {
+        Modal.getOrCreateInstance(dialogElem.value, config)
+    }
 })
+
+const emit = defineEmits<{
+    (e: 'submit'): void
+    (e: 'abort'): void
+}>()
+
+function onAbort() {
+    emit('abort')
+
+    if (props.autoCloseOnAbort) {
+        close()
+    }
+}
+
 </script>
 
 <template>
     <div class="modal" ref="dialogElem">
         <div class="modal-dialog">
             <div class="modal-content">
-                <div class="modal-header">
+                <div class="modal-header" v-if="props.withHeader">
                     <h5 class="modal-title">
                         <slot name="title"></slot>
                     </h5>
@@ -37,8 +85,10 @@ onMounted(() => {
                     <slot name="body"></slot>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" @click="close">Close</button>
-                    <button type="button" class="btn btn-primary" @click="close">Save changes</button>
+                    <button type="button" class="btn btn-secondary" @click="onAbort">{{ t(props.cancelCaption)
+                    }}</button>
+                    <button type="button" class="btn btn-primary" @click="$emit('submit')">{{ t(props.submitCaption)
+                    }}</button>
                 </div>
             </div>
         </div>
