4430aa578c03da7e220b6796f8b92fef9159e234
Philipp Jeske <philipp.jeske@wasserwacht.bayern>
2026-08-01 21:44:42 +0200

    Fix 3: Add dismissible error alert display in SentryCard using error ref from store

--- digital-guardbook-ui/src/components/sentry/SentryCard.vue
+++ digital-guardbook-ui/src/components/sentry/SentryCard.vue
@@ -1,59 +1,70 @@
 <script setup lang="ts">
-
-import SentryDisplay from './SentryDisplay.vue';
-import SentryStart from './SentryStart.vue';
-import GuardsList from './GuardsList.vue';
-import LogBookEntryList from '@/components/logBook/LogBookEntryList.vue';
+import SentryDisplay from './SentryDisplay.vue'
+import SentryStart from './SentryStart.vue'
+import GuardsList from './GuardsList.vue'
+import LogBookEntryList from '@/components/logBook/LogBookEntryList.vue'
 import { useSentryStore } from '@/store/sentry'
 import { storeToRefs } from 'pinia'
 
 // const sentry: Ref<Sentry | null> = ref(null)
 
 import { useI18n } from 'vue-i18n'
-import { onMounted } from 'vue';
+import { onMounted } from 'vue'
 
 const { t } = useI18n({
-    useScope: 'global'
+  useScope: 'global'
 })
 
 const store = useSentryStore()
 
-const { active, loading, sentry} = storeToRefs(store)
+const { active, loading, sentry, error } = storeToRefs(store)
+
+const clearError = () => {
+  error.value = null
+}
 
 onMounted(() => {
-    store.getActiveSentry()
+  store.getActiveSentry()
 })
-
 </script>
 
 <template>
-    <div :class="{ 'col-md-6': active, 'col-md-9': !active }">
-        <template v-if="!loading">
-            <div class="card">
-                <div class="card-body">
-                    <SentryDisplay v-model:sentry="sentry" v-if="active && sentry" />
-                    <SentryStart v-else @created="sentry = $event" />
-                </div>
-            </div>
-            <div class="card" v-if="active">
-                <div class="card-body">
-                    <LogBookEntryList v-model:from="sentry.start" v-if="sentry"/>
-                </div>
-            </div>
-        </template>
+  <div :class="{ 'col-md-6': active, 'col-md-9': !active }">
+    <div v-if="error" class="alert alert-danger alert-dismissible fade show" role="alert">
+      {{ error }}
+      <button type="button" class="btn-close" @click="clearError"></button>
     </div>
-    <div class="col-md-3" v-if="active && sentry">
-        <div class="card">
-            <div class="card-body">
-                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-circle-fill" fill="currentColor"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <circle cx="8" cy="8" r="8" />
-                </svg>
-                {{ t('sentry.activeGuards') }}
-
-                <GuardsList v-model:guards="sentry.guards" />
-
-            </div>
+    <template v-if="!loading">
+      <div class="card">
+        <div class="card-body">
+          <SentryDisplay v-model:sentry="sentry" v-if="active && sentry" />
+          <SentryStart v-else @created="sentry = $event" />
         </div>
+      </div>
+      <div class="card" v-if="active">
+        <div class="card-body">
+          <LogBookEntryList v-model:from="sentry.start" v-if="sentry" />
+        </div>
+      </div>
+    </template>
+  </div>
+  <div class="col-md-3" v-if="active && sentry">
+    <div class="card">
+      <div class="card-body">
+        <svg
+          width="1em"
+          height="1em"
+          viewBox="0 0 16 16"
+          class="bi bi-circle-fill"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <circle cx="8" cy="8" r="8" />
+        </svg>
+        {{ t('sentry.activeGuards') }}
+
+        <GuardsList v-model:guards="sentry.guards" />
+      </div>
     </div>
+  </div>
 </template>
